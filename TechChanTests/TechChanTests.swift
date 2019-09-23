182801e40d70116cf733790d90534420db2d349e
otakumi <otakumi@m.cs.osakafu-u.ac.jp>
2019-09-23 16:04:19 +0900

    Initial Commit

---
+++ TechChanTests/TechChanTests.swift
@@ -0,0 +1,34 @@
+//
+//  TechChanTests.swift
+//  TechChanTests
+//
+//  Created by 大林拓実 on 2019/09/23.
+//  Copyright © 2019 大林拓実. All rights reserved.
+//
+
+import XCTest
+@testable import TechChan
+
+class TechChanTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
